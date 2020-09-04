396071e0cc1eb499bdfddb4fffff9c6f4dc244c8
Joshua Cid <joshua.cid@virginiogomez.cl>
2020-09-04 16:16:51 -0400

    First commit

---
+++ public/precache-manifest.87cbb67bc2ecc66cafcc4eddec992f79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e1c5da197c48061a267fe37d91cb1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceaf4a1a450d53102fd4",
+    "url": "/static/css/2.ceee16bc.chunk.css"
+  },
+  {
+    "revision": "96b568c31f12026176a0",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "ceaf4a1a450d53102fd4",
+    "url": "/static/js/2.eb329115.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.eb329115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96b568c31f12026176a0",
+    "url": "/static/js/main.35076836.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
